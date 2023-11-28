73a44bfba8d0a2a465fdae021e3ad1638902ad27
Farhan <raditzfarhan@gmail.com>
2023-11-28 16:25:04 +0800

    Add order get after request

--- src/Services/OrderService.php
+++ src/Services/OrderService.php
@@ -27,4 +27,18 @@ class OrderService extends BaseService
             }
         }
     }
+
+    public function afterGetRequest(LazadaMessage $request, array $result = []): void
+    {
+        $data = data_get($result, 'data');
+        $order_id = data_get($data, 'order_id');
+
+        if ($order_id) {
+            LazadaOrder::updateOrCreate([
+                'id' => $order_id
+            ], [
+                'statuses' =>  data_get($data, 'statuses'),
+            ]);
+        }
+    }
 }
